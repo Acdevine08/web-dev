c126ca23d0fc9c7302f1bf53f9be8bb40fad2613
Acdevine08 <158475291+Acdevine08@users.noreply.github.com>
2024-04-15 09:03:44 -0400

    JSP1

---
+++ Practice 1/script.js
@@ -0,0 +1,13 @@
+let cup = 5;
+console.log(cup);
+
+cup += 5;
+console.log(cup);
+
+const add_this = 2;
+
+function incrementNumber(initial,incrementor) {
+    return initial + incrementor;
+}
+
+console.log(incrementNumber(cup, add_this))
