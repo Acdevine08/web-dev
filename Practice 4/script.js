4a87c79ec096046230ad6e7695db17dcd96d91e8
Acdevine08 <158475291+Acdevine08@users.noreply.github.com>
2024-05-14 07:17:48 -0400

    m

---
+++ Practice 4/script.js
@@ -0,0 +1,17 @@
+class Shape{
+    x;
+    y;
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    printValues() {
+        console.log("x: "+this.x)
+        console.log("y: "+this.y)
+    }
+}
+
+class Main{
+    constructor() {}
+}
+let main = new Main();
